e5d963d8002486385750ac702ed8876f4864b0f9
Christoph <c.wagner.germany@gmail.com>
2023-12-22 18:55:54 +0100

    add input2

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Ingredient } from '../shared/ingridient.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
